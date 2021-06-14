85da51308c891e5ebe85270f03c3e3f790cf491c
daniyani <daniyanii@yandex.ru>
2021-06-14 03:31:00 +0300

    create dropping menu & fixed responsive version

--- js/main.js
+++ js/main.js
@@ -3,6 +3,28 @@ $('.slider').slick({
   infinite: true,
   speed: 300,
   slidesToShow: 1,
-  variableWidth: true
+  variableWidth: true,
+  speed: 500,
+  easing: 'ease',
+ 
+
 });
 });
+
+
+
+var menuButton = document.querySelector(".burger");
+menuButton.addEventListener("click", function (){
+document
+.querySelector(".drop-menu")
+.classList.toggle("drop-menu--visible");
+});
+
+var closeButton = document.querySelector(".drop-menu__close");
+closeButton.addEventListener("click", function (event){
+  event.preventDefault();
+document
+var menuClose = $(".drop-menu");
+menuClose.removeClass ("drop-menu--visible")
+});
+
