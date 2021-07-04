eda6d6af1a1b77c6fa039a75aeddba56ddffa406
daniyani <daniyanii@yandex.ru>
2021-07-04 21:33:16 +0300

    added phpmailer

--- js/main.js
+++ js/main.js
@@ -1,64 +1,95 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true,
+    speed: 500,
+    easing: 'ease',
 
-$(document).ready(function(){
-$('.slider').slick({
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  variableWidth: true,
-  speed: 500,
-  easing: 'ease',
 
 
+  });
+
+
+
+  var modalButton = $('.burger');
+  var modalClose = $('.close');
+  var closeOverlay = $('.overlay')
+  modalButton.on("click", openModal);
+  modalClose.on("click", closeModal);
+  function openModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".overlay");
+    var modalMenu = $(".drop-menu");
+    var logoOpacity = $(".opacity-logo")
+    var closeOpacity = $(".opacity-close")
+    modalOverlay.addClass("overlay--visible");
+    modalMenu.addClass("drop-menu--visible");
+    logoOpacity.addClass("opacity-logo--visible")
+    closeOpacity.addClass("opacity-close--visible")
+
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".overlay");
+    var modalMenu = $(".drop-menu");
+    modalOverlay.removeClass("overlay--visible");
+    modalMenu.removeClass("drop-menu--visible");
+  }
+
+  $(document).on('keydown', function (e) {
+    if (e.which === 27) {
+      var modalOverlay = $(".overlay");
+      var modalDialog = $(".drop-menu");
+      modalOverlay.removeClass("overlay--visible")
+      modalDialog.removeClass("drop-menu--visible")
+
+    }
+  });
+
+  $(document).click(function (event) {
+    if (!$(event.target).closest(".drop-menu, [data-toggle=modal]").length) { $("body").find(".overlay").removeClass("overlay--visible"); $('body').removeClass("overflow") }
+  });
+
+  $(document).click(function (event) {
+    if (!$(event.target).closest(".drop-menu, [data-toggle=modal]").length) { $("body").find(".drop-menu").removeClass("drop-menu--visible"); $('body').removeClass("overflow") }
+  });
+
+
+  $('.hero__link').hover(
+    function () { $(this).addClass('hero__link--active') },
+    function () { $(this).removeClass('hero__link--active') }
+  );
+
+
+
+  // let validateForms = function (selector, rules, seccessModal, yaGoal) {
+  //   new window.JustValidate(selector, {
+  //     rules: rules,
+  //     submitHandler: function(form){
+  //       let formData = new FormData(form);
+  //       let xhr = new XMLHttpRequest();
+
+  //       xhr.onreadystatechange = function () {
+  //         if (xhr.readyState === 4) {
+  //           if (xhr.status === 200) {
+  //             console.log('Отправлено');
+  //           }
+  //         }
+  //       }
+
+  //       xhr.open('POST', 'send.php', true);
+  //       xhr.send(formData);
+
+  //       form.reset();
+  //     }
+  //   });
+  // }
+
+  // validateForms('.about-form', {email:{required: true, email: true}, name: {required: true, minlength: 3}, comment: {required: true}}, )
 
 });
-});
 
 
-var modalButton = $('.burger');
-var modalClose = $('.close');
-var closeOverlay = $('.overlay')
-modalButton.on("click", openModal);
-modalClose.on("click", closeModal);
-function openModal (event) {
-  event.preventDefault();
-var modalOverlay = $(".overlay");
-var modalMenu = $(".drop-menu");
-var logoOpacity = $(".opacity-logo")
-var closeOpacity = $(".opacity-close")
-modalOverlay.addClass("overlay--visible");
-modalMenu.addClass("drop-menu--visible");
-logoOpacity.addClass ("opacity-logo--visible")
-closeOpacity.addClass ("opacity-close--visible")
 
-}
-function closeModal (event) {
-event.preventDefault();
-var modalOverlay = $(".overlay");
-var modalMenu = $(".drop-menu");
-modalOverlay.removeClass("overlay--visible");
-modalMenu.removeClass("drop-menu--visible");
-}
-
-$(document).on('keydown', function(e){
-if(e.which === 27){
-var modalOverlay = $(".overlay");
-var modalDialog = $(".drop-menu");
-modalOverlay.removeClass ("overlay--visible")
-modalDialog.removeClass ("drop-menu--visible")
-
-}
-});
-
-$(document).click(function(event)
-{if(!$(event.target).closest(".drop-menu, [data-toggle=modal]").length)
-{$("body").find(".overlay").removeClass("overlay--visible");$('body').removeClass("overflow")}});
-
-$(document).click(function(event)
-{if(!$(event.target).closest(".drop-menu, [data-toggle=modal]").length)
-{$("body").find(".drop-menu").removeClass("drop-menu--visible");$('body').removeClass("overflow")}});
-
-
-$('.hero__link').hover(
-       function(){ $(this).addClass('hero__link--active') },
-       function(){ $(this).removeClass('hero__link--active') }
-);
---
+++ send.php
@@ -0,0 +1,57 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$email = $_POST['email'];
+$name = $_POST['name'];
+$message = $_POST['message'];
+
+$title = "Heaven Production";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Электронная почта:</b> $email<br><br>
+<b>Сообщение:</b><br>$message
+";
+
+
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'abouthvnprdctn@gmail.com'; // Логин на почте
+    $mail->Password   = 'Heavenproduction123.'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('abouthvnprdctn@gmail.com', 'Heaven Production'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('daniyanii@yandex.ru');
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
